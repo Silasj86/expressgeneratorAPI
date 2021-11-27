d2dc22e2fa28ae21def3027a127dec1d86ec250b
Silas <silas.johnson86@gmail.com>
2021-11-27 06:57:24 -0600

    CORS

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
-
+const cors = require('./cors');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+router.get('/',cors.corsWithOptions,  authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
 
   User.find()
     .then((users) => {
@@ -18,7 +18,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req
   
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup',cors.corsWithOptions,  (req, res) => {
   User.register(
       new User({username: req.body.username}),
       req.body.password,
@@ -52,7 +52,7 @@ router.post('/signup', (req, res) => {
   );
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   const token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
@@ -60,7 +60,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
 });
 
 
-router.get('/logout', (req, res, next) => {
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
